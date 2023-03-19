ec8334dcb19254b324ab0c707ea2d4ab6e90b7fa
stefan <moogs@gmx.de>
2023-03-19 21:50:03 +0100

    Add deploy script

---
+++ R/deploy.R
@@ -0,0 +1,6 @@
+# Authenticate
+setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
+               token = Sys.getenv("TOKEN"),
+               secret = Sys.getenv("SECRET"))
+# Deploy
+deployApp(appFiles = c("app.R", "R/km6-chart.R", "data/km6.rds"))
